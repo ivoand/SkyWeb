8281f0402f25e9590de6b32bedaae1b27654f4a2
ivoand <chanderslv@gmail.com>
2022-02-04 16:53:03 +0200

    first part

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.btn-1').click(function() {
+        $('.det-expl-1').toggle('after');
+        $('.arrow-down').toggle()
+        $('.arrow-up').toggle()
+    })
+
+
+
+
+});
