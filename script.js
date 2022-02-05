9bf93dca8851183504ba96321d32111224b1e480
ivoand <chanderslv@gmail.com>
2022-02-05 12:47:10 +0200

    update details with yellow line

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ $(".btn-1").click(function() {
     $('.det-expl-1').toggle(500);
     $('.arrow-down-1').toggle()
     $('.arrow-up-1').toggle()
+    $('.det1').toggleClass('details-line')
 })
 $(".btn-2").click(function() {
-    $('.det-expl-2').toggle(500);
+    $('.det-expl-2').toggle(500)
     $('.arrow-down-2').toggle()
     $('.arrow-up-2').toggle()
+    $('.det2').toggleClass('details-line')
+
 })
 $(".btn-3").click(function() {
     $('.det-expl-3').toggle(500);
     $('.arrow-down-3').toggle()
     $('.arrow-up-3').toggle()
+    $('.det3').toggleClass('details-line')
 })
 $(".btn-4").click(function() {
     $('.det-expl-4').toggle(500);
     $('.arrow-down-4').toggle()
     $('.arrow-up-4').toggle()
+    $('.det4').toggleClass('details-line')
 })
 $(".menu-icon").click(function() {
     $('.menu-icon-on').toggle()
