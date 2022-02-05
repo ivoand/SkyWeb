8a97c0191399196ea017f1adb98ffa805f2b6889
ivoand <chanderslv@gmail.com>
2022-02-05 11:45:48 +0200

    finish

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-$(document).ready(function() {
-    $('.btn-1').click(function() {
-        $('.det-expl-1').toggle('after');
-        $('.arrow-down').toggle()
-        $('.arrow-up').toggle()
-    })
-
-
-
-
-});
+$(".btn-1").click(function() {
+    $('.det-expl-1').toggle(500);
+    $('.arrow-down-1').toggle()
+    $('.arrow-up-1').toggle()
+})
+$(".btn-2").click(function() {
+    $('.det-expl-2').toggle(500);
+    $('.arrow-down-2').toggle()
+    $('.arrow-up-2').toggle()
+})
+$(".btn-3").click(function() {
+    $('.det-expl-3').toggle(500);
+    $('.arrow-down-3').toggle()
+    $('.arrow-up-3').toggle()
+})
+$(".btn-4").click(function() {
+    $('.det-expl-4').toggle(500);
+    $('.arrow-down-4').toggle()
+    $('.arrow-up-4').toggle()
+})
+$(".menu-icon").click(function() {
+    $('.menu-icon-on').toggle()
+    $('.menu-icon-off').toggle()
+    $('#menu').toggle(500)
+})
